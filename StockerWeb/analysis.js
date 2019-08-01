b2713c370ad58d14f646fe4b75adf25e2013c78c
shkim <kimsh@wemb.co.kr>
2019-08-01 18:59:53 +0900

    머신 러닝 추가

--- StockerWeb/analysis.js
+++ StockerWeb/analysis.js
@@ -1,6 +1,7 @@
 const _ = require('lodash');
 const si = require('systeminformation');
 const tf = require('@tensorflow/tfjs')
+const nj = require('numjs');
 const knex = require('knex');
 require('@tensorflow/tfjs-node');
 require('@tensorflow/tfjs-node-gpu');
@@ -42,7 +43,7 @@ var csv = {
 var utils = require('./machine_learning/utils/utils.js')
 
 // [{key:'category',condition:'=',value:'009150'},{key:'unixtime',condition:'<=',value:'2019-01-22'}]
-khan.model.past_stock.selectByCategory([{key:'category',condition:'=',value:'085310'},{key:'unixtime',condition:'<=',value:'2019-06-18'}]).map((row) => {
+khan.model.past_stock.selectByCategory([{key:'category',condition:'=',value:'085310'}]).map((row) => {
     row.rawdata = JSON.parse(row.rawdata);
     csv.volume.push(parseFloat(row.rawdata["거래량"]));
     csv.date.push(moment(row.unixtime).format('YYYY-MM-DD'));
@@ -63,12 +64,113 @@ khan.model.past_stock.selectByCategory([{key:'category',condition:'=',value:'085
     var stock_date = csv.date;
     var volume = csv.volume;
 
-    var test = utils.minmax_1d(close)
-    tf.tensor3d([1, 2, 3, 4], [2, 2, 1]).print();
+    var minmax_scaled = utils.minmax_1d(close)
+    var timestamp = 10;
+    var epoch = 50;
+    var future = 20;
+    var layer_size = 32;
+    var learning_rate = 0.01;
+    var smooth = 0.1;
+    var X_scaled = minmax_scaled.scaled.slice([0], [Math.floor(minmax_scaled.scaled.shape[0]/timestamp)*timestamp+1])
 
-    fsPath.writeFile('./test.json', JSON.stringify(csv), 'utf-8', function(){
-        console.log('write!!!')
-    });
+    var cells = [tf.layers.lstmCell({units:layer_size})];
+    var rnn = tf.layers.rnn({cell:cells, returnSequences: true, returnState : true});
+    var dense_layer = tf.layers.dense({units:1, activation: 'linear'});
+
+    var in_dropout_rate = 1;
+    var out_dropout_rate = 0.8;
+    function f(x,states) {
+        try {
+            x = utils.dropout_nn(x, in_dropout_rate);
+            var forward = rnn.apply(x, {initialState:states});
+            var last_sequences = utils.dropout_nn(forward[0].reshape([x.shape[1], layer_size]), out_dropout_rate);
+            return {'forward' : dense_layer.apply(last_sequences), 'state_1': forward[1], 'state_2':forward[2]};   
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    var cost = (label, pred) => tf.square(tf.sub(label, pred)).mean();
+    var optimizer = tf.train.adam(learning_rate);
+    var batch_states = [tf.zeros([1, layer_size]), tf.zeros([1, layer_size])];
+    var arr_loss = [];
+    var arr_layer = [];
+
+    var learning_result = {};
+
+    function async_training_loop(callback) {
+        (function loop(i) {
+            var total_loss = 0;
+            for(var k = 0; k < Math.floor(X_scaled.shape[0]/timestamp)*timestamp; k+=timestamp) {
+                batch_x = X_scaled.slice([k],[timestamp]).reshape([1,-1,1])
+                batch_y = X_scaled.slice([k+1],[timestamp]).reshape([-1,1])
+                feed = f(batch_x,batch_states)
+                optimizer.minimize(() => cost(batch_y,f(batch_x,batch_states)['forward']));
+                total_loss += parseFloat(cost(batch_y,f(batch_x,batch_states)['forward']).toString().slice(7));
+                batch_states = [feed.state_1,feed.state_2]
+            }
+
+            total_loss /= Math.floor(X_scaled.shape[0]/timestamp);
+            arr_loss.push(total_loss)
+            output_predict = nj.zeros([X_scaled.shape[0]+future, 1])
+            output_predict.slice([0,1],null).assign(utils.tf_str_tolist(X_scaled.slice(0,1))[0],false)
+            upper_b = Math.floor(X_scaled.shape[0]/timestamp)*timestamp
+            distance_upper_b = X_scaled.shape[0] - upper_b
+            batch_states = [tf.zeros([1,layer_size]),tf.zeros([1,layer_size])];
+
+            for(var k = 0; k < (Math.floor(X_scaled.shape[0]/timestamp)*timestamp); k+=timestamp){
+                batch_x = X_scaled.slice([k],[timestamp]).reshape([1,-1,1])
+                feed = f(batch_x,batch_states)
+                state_forward = utils.tf_nj_list(feed.forward)
+                output_predict.slice([k+1,k+1+timestamp],null).assign(state_forward,false)
+                batch_states = [feed.state_1,feed.state_2]
+            }
+
+            batch_x = X_scaled.slice([upper_b],[distance_upper_b]).reshape([1,-1,1])
+            feed = f(batch_x,batch_states)
+            state_forward = utils.tf_nj_list(feed.forward)
+            output_predict.slice([upper_b+1,X_scaled.shape[0]+1],null).assign(state_forward,false)
+            pointer = X_scaled.shape[0]+1
+            tensor_output_predict = output_predict.reshape([-1]).tolist()
+            batch_states = [feed.state_1,feed.state_2];
+
+            for(var k = 0; k < future-1; k+=1){
+                batch_x = tf.tensor(tensor_output_predict.slice(pointer-timestamp,pointer)).reshape([1,-1,1])
+                feed = f(batch_x,batch_states)
+                state_forward = utils.tf_nj_list(feed.forward.transpose())
+                tensor_output_predict[pointer] = state_forward[0][4]
+                pointer += 1
+                batch_states = [feed.state_1,feed.state_2]
+            }
+            console.log( 'Epoch: '+(i+1)+', avg loss: '+total_loss);
+
+            var predicted_val = utils.tf_nj_list_flatten(utils.reverse_minmax_1d(tf.tensor(tensor_output_predict),minmax_scaled['min'],minmax_scaled['max']));
+            var predicted_val = utils.smoothing_line(predicted_val,smooth);
+            console.log('Predict Result :', predicted_val.length);
+            learning_result["epoch"+i+1] = {
+                loss:total_loss,
+                predicted: predicted_val
+            }
+
+            if(i < (epoch - 1)) {
+                loop(++i)
+            } else {
+                callback(predicted_val);
+            }
+
+        }(0))
+    }
+
+    async_training_loop(function(predicted) {
+        console.log('Done Training');
+        console.log(predicted[predicted.length-1] + '원')
+    })
+
+    //console.log(Math.floor(minmax_scaled.scaled.shape[0]/timestamp)*timestamp+1);
+    // tf.tensor3d([1, 2, 3, 4], [2, 2, 1]).print();
+
+    // fsPath.writeFile('./test.json', JSON.stringify(csv), 'utf-8', function(){
+    //     console.log('write!!!')
+    // });
     
     // const model = tf.sequential();
     // model.add(tf.layers.dense({units:1, inputShape:[1]}));
--- StockerWeb/machine_learning/utils/utils.js
+++ StockerWeb/machine_learning/utils/utils.js
@@ -1,5 +1,6 @@
 var instance;
 const tf = require('@tensorflow/tfjs')
+const nj = require('numjs')
 require('@tensorflow/tfjs-node');
 require('@tensorflow/tfjs-node-gpu');
 
@@ -14,6 +15,46 @@ function Utils() {
         a_max = tf_str_tolist(a.max())
         scaled = tf.div(tf.sub(a,a.min()), tf.sub(a.max(),a.min()))
         return {'scaled':scaled,'min':a_min,'max':a_max}
+    }
+
+    this.dropout_nn = function (x,keep_prob){
+        uniform = tf.randomUniform(x.shape)
+        added = tf.add(tf.scalar(keep_prob),uniform)
+        binary = tf.floor(added)
+        return tf.mul(tf.div(x,tf.scalar(keep_prob)),binary)
+    }
+
+    this.tf_nj_list = function (a){
+        var arr = nj.zeros([a.shape[0],a.shape[1]]).tolist();
+        for(var i = 0; i < a.shape[0];i++){
+          for(var k = 0; k < a.shape[1];k++) arr[i][k] = JSON.parse(a.slice([i,k],[1,1]).toString().slice(7).trim().replace(',',''))[0][0]
+        }
+        return arr
+    }
+
+    this.tf_nj_list_flatten = function (a){
+        var arr = nj.zeros([a.shape[0]]).tolist();
+        for(var i = 0; i < a.shape[0];i++) arr[i] = JSON.parse(a.slice([i],[1]).toString().slice(7).trim())[0]
+        return arr
+    }
+
+    this.tf_str_tolist = function (a){
+        return JSON.parse(a.toString().slice(7).trim())
+    }
+
+    this.reverse_minmax_1d = function (a, a_min, a_max){
+        return tf.add(tf.mul(a, tf.scalar(a_max-a_min)), tf.scalar(a_min))
+      }
+
+    this.smoothing_line = function (scalars,weight){
+        var last = scalars[0]
+        smoothed = []
+        for(var i = 0; i < scalars.length;i++){
+            smoothed_val = last * weight + (1 - weight) * scalars[i]
+            smoothed.push(smoothed_val)
+            last = smoothed_val
+        }
+        return smoothed
     }
 }
 
